bd4ccdb066a0b726784604698aef3768476afc78
cullyn-inverba <cullyn.newman@gmail.com>
2020-08-14 22:08:36 -0700

    shell a bit better this time. Added icons and a breakpoint for mobile. Still no need for sidenav. Need to figure out how to display page still.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
 import { ShellComponent } from './shell/shell.component';
 
 
+const components = [ShellComponent];
+
+const modules = [
+  CommonModule,
+  LayoutModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
+];
 
 @NgModule({
-  declarations: [ShellComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [ShellComponent]
+  declarations: [...components],
+  imports: [...modules],
+  exports: [
+    ...components,
+    ...modules,
+  ]
 })
 export class SharedModule { }

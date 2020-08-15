87ef81fbba6616fb085ce71ed69b6a818eda3549
cullyn-inverba <cullyn.newman@gmail.com>
2020-08-15 16:38:15 -0700

    Added 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '404', component: PageNotFoundComponent},
   {path: 'home', component: HomeComponent},
   {path: 'research', loadChildren: () => import('./research/research.module').then(m => m.ResearchModule)},
   {path: 'works', loadChildren: () => import('./works/works.module').then(m => m.WorksModule)},
   {path: 'education', loadChildren: () => import('./education/education.module').then(m => m.EducationModule)},
   {path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)},
+  {path: '**', redirectTo: '/404', pathMatch: 'full'},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,

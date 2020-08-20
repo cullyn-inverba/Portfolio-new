f8e2b00c119da1d736d3eddc9224406d13cd76de
cullyn-inverba <cullyn.newman@gmail.com>
2020-08-19 19:21:45 -0700

    Attemping to set up sendgrid function... saving place before I try adding function call to app.component

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,36 @@
-// import * as functions from 'firebase-functions';
+// Firebase Config
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+const db = admin.firestore();
+const email = 'cullyn@cullyn.io'
+
+// Sendgrid Config
+import * as sgMail from '@sendgrid/mail';
+
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
+sgMail.setApiKey(API_KEY);
+
+export const newContact = functions.firestore.document('contacts/{contactID}/').onCreate( async (change, context) => {
+    
+    const contactSnap = await db.collection('contacts').doc(context.params.contactId).get();
+
+    const contact = contactSnap.data() || {};
+
+    const msg = {
+        to: email,
+        from: 'contact@cullyn.io',
+        templateId: TEMPLATE_ID,
+        dynamic_template_data: {
+            subject: `New contact from ${contact.name}`,
+            preheader: `From ${contact.email}`,
+            name: contact.name,
+            message: contact.message,
+            email: contact.email
+        },
+    };
+
+    return sgMail.send(msg);
+});
